a8104a35262e1f9ab735c7070ee70312b09f555b
stefanRachev <starfystef@gmail.com>
2025-02-21 17:49:29 +0200

    Setup Express server with MongoDB connection and CORS configuration

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
-
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config();
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
 });
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.log('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
