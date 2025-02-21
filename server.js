019076888e6f9fbcf43c532b8908079d07615b9f
stefanRachev <starfystef@gmail.com>
2025-02-21 18:00:30 +0200

    Refactoring the MongoDB connection and removing deprecated options

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -21,10 +22,7 @@ app.get("/", (req, res) => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI, {})
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
