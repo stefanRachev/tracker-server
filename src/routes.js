c86be8640013280ced6d3914a352fef49676deb0
stefanRachev <starfystef@gmail.com>
2025-02-22 09:50:42 +0200

    fix import path on Item Schema

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Item = require("./models/Item");
+const Item = require("./models/Items");
 
 
 router.post("/items", async (req, res) => {
