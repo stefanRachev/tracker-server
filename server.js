2edde5422d65fde6f1c5b5b960fb1d14bd1bf4d5
stefanRachev <starfystef@gmail.com>
2025-02-22 09:33:33 +0200

    config routes

--- server.js
+++ server.js
@@ -2,29 +2,35 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const itemRoutes = require("./src/routes");
 
-const app = express();
 const port = process.env.PORT || 5001;
 
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+  
 const corsOptions = {
-  origin: ["http://localhost:3000","http://localhost:5173"] ,
+  origin: ["http://localhost:3000", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
 });
 
-mongoose
-  .connect(process.env.MONGO_URI, {})
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("MongoDB connection error:", err));
+
+app.use("/api", itemRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
