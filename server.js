10e2871098b230a3d0acd99fedd88377e098b868
stefanRachev <starfystef@gmail.com>
2025-02-23 19:39:59 +0200

    adding the new address to the cors origin

--- server.js
+++ server.js
@@ -15,20 +15,22 @@ mongoose
   .connect(process.env.MONGO_URI, {})
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("MongoDB connection error:", err));
-  
+
 const corsOptions = {
   origin: ["http://localhost:3000", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "https://tracker-server-xob4.onrender.com",
+  ],
 };
 
 app.use(cors(corsOptions));
 
-
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
 });
-
 
 app.use("/api", itemRoutes);
 
