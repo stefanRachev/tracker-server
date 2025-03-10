c5010ea19ebdffe8ef18e9fd392498ad91ffbce9
stefanRachev <starfystef@gmail.com>
2025-03-10 21:45:22 +0200

    set log

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const routes = require("./src/routes")
+const routes = require("./src/routes");
 const mongoose = require("mongoose");
 const path = require("path");
 const { title } = require("process");
@@ -23,7 +23,7 @@ mongoose
 
 const corsOptions = {
   origin: [
-    "http://localhost:3000",  
+    "http://localhost:3000",
     "http://localhost:5173",
     "https://tracker-server-xob4.onrender.com",
     "https://expense-tracker-olive-one.vercel.app",
@@ -36,7 +36,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
-  
 app.use("/api", routes);
 
 app.use(express.static(path.join(__dirname, "..", "tracker-client", "dist")));
@@ -45,17 +44,16 @@ app.get("/", (req, res) => {
   res.render("index", {
     title: "Expense Tracker",
     description: "Track your expenses and manage your budget effortlessly.",
-    image: "/path/to/your/image.jpg",
+    image: "/images/logo.svg",
     url: "https://expense-tracker-olive-one.vercel.app/",
   });
 });
 
-
-
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "tracker-client", "dist", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "..", "tracker-client", "dist", "index.html")
+  );
 });
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
