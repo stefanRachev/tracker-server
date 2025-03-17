e94aba722da8cbb9737876a7b495b74d9dd5829e
stefanRachev <starfystef@gmail.com>
2025-03-17 14:27:57 +0200

    fixed url

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose
   .catch((err) => console.log("MongoDB connection error:", err));
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://tracker-client-zeta.vercel.app/"],
+  origin: ["http://localhost:5173", "https://tracker-client-zeta.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
