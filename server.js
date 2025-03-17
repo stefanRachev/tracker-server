0a96b643854d8ed4d5211531d394347971871f5f
stefanRachev <starfystef@gmail.com>
2025-03-17 13:37:42 +0200

    test

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const corsOptions = {
   origin: ["http://localhost:5173", "https://tracker-client-hazel.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
 };
 
 app.use(cors(corsOptions));
