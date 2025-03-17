de0a09375986179c32cf943c628563298fe4d491
stefanRachev <starfystef@gmail.com>
2025-03-17 13:36:18 +0200

    test

--- server.js
+++ server.js
@@ -2,17 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const routes = require("./src/routes");
+const app = express();
 const mongoose = require("mongoose");
 
-
-
 //const port = process.env.PORT || 5001;
-
-const app = express();
-
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI, {})
@@ -20,10 +13,7 @@ mongoose
   .catch((err) => console.log("MongoDB connection error:", err));
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://tracker-client-hazel.vercel.app",
-  ],
+  origin: ["http://localhost:5173", "https://tracker-client-hazel.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -31,18 +21,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/api", routes);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello from Vercel Serverless!');
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel Serverless!");
 });
 
-
-module.exports = app; 
+module.exports = app;
 
 // app.listen(port, () => {
 //   console.log(`Server running on port ${port}`);
