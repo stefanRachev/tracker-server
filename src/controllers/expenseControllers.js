9a1d0e7c6b295cb8ee70f1fd8dad3d7a563bd087
stefanRachev <starfystef@gmail.com>
2025-03-17 22:05:45 +0200

    adding pagination to expense controller

--- src/controllers/expenseControllers.js
+++ src/controllers/expenseControllers.js
@@ -29,7 +29,7 @@ exports.addExpense = async (req, res) => {
 };
 
 exports.getExpenses = async (req, res) => {
-  const { userId, description, category, startDate, endDate } = req.query;
+  const { userId, description, category, startDate, endDate, page = 1, limit = 10 } = req.query;
 
   if (!userId) {
     return res.status(400).json({ message: "UserId is required." });
@@ -59,14 +59,20 @@ exports.getExpenses = async (req, res) => {
     filters.createdAt = { ...filters.createdAt, $lte: new Date(endDate) };
   }
 
+  const pageNumber = parseInt(page) || 1;
+  const pageSize = parseInt(limit) || 10;
+  const skip = (pageNumber - 1) * pageSize;
+
   try {
-    const expenses = await Expense.find(filters).sort({ createdAt: -1 });
+    const expenses = await Expense.find(filters)
+      .sort({ createdAt: -1 }) 
+      .skip(skip)
+      .limit(pageSize);
 
-    if (expenses.length === 0) {
-      return res.status(404).json({ message: "No expenses found." });
-    }
+    const totalExpenses = await Expense.countDocuments(filters); 
+    const hasMore = totalExpenses > pageNumber * pageSize; 
 
-    res.status(200).json(expenses);
+    res.status(200).json({ expenses, hasMore });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error fetching expenses." });
