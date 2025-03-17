c26640a66dab8631d4b466d06eb303ed94d4f140
stefanRachev <starfystef@gmail.com>
2025-03-17 13:40:44 +0200

    test

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const routes = require("./src/routes");
+const routes = require("../src/routes");
 const app = express();
 const mongoose = require("mongoose");
 
