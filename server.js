b1f621e92b9f821f8dd798377f9115e8c4ec5c19
stefanRachev <starfystef@gmail.com>
2025-03-20 01:07:35 +0200

    test cookie proxy

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const routes = require("./src/routes");
 const app = express();
 const mongoose = require("mongoose");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const port = process.env.PORT || 5001;
 
@@ -20,17 +22,45 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api", routes);
 
+app.get("/api/check-cookie", (req, res) => {
+  const testCookie = req.cookies.test_cookie;
+
+  if (testCookie) {
+    res.status(200).json({
+      status: "success",
+      message: "Cookie found!",
+      cookieValue: testCookie,
+    });
+  } else {
+    res.status(400).json({
+      status: "fail",
+      message: "No cookie found.",
+    });
+  }
+});
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://tracker-server-r17d.onrender.com",
+    changeOrigin: true,
+    secure: false,
+    onProxyReq: (proxyReq, req, res) => {
+      proxyReq.setHeader("Origin", "https://tracker-client-zeta.vercel.app");
+    },
+  })
+);
+
 app.get("/", (req, res) => {
   res.send("Hello from Vercel Serverless!");
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
